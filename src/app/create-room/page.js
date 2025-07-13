8cb8281c2aaac048ee0cd556c3f3e79ce4216a4e
kennethgo <gennethko@gmail.com>
2025-07-13 13:21:45 +1000

    Added base home page with shadcn UI components

---
+++ src/app/create-room/page.js
@@ -0,0 +1,11 @@
+'use client';
+import Link from 'next/link';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function CreateRoom() {
+  return (
+    <div className="min-h-screen flex items-center bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
+        <p>CREATE ROOM</p>
+    </div>
+  );
+}
---
+++ src/app/join-room/page.js
@@ -0,0 +1,11 @@
+'use client';
+import Link from 'next/link';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function JoinRoom() {
+  return (
+    <div className="min-h-screen flex items-center bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
+        <p>JOIN ROOM</p>
+    </div>
+  );
+}
